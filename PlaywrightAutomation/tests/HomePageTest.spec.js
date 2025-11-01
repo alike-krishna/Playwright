6fc842e995aaca467041b115fcf5303178257588
alike-krishna <alike.krishna@gmail.com>
2025-11-01 19:30:52 +0530

    Update HomePageTest.spec.js

--- PlaywrightAutomation/tests/HomePageTest.spec.js
+++ PlaywrightAutomation/tests/HomePageTest.spec.js
@@ -2,9 +2,9 @@ const { test, expect } = require('@playwright/test');
 
 test('Home Page',async({page})=>{
 
-    await page.goto('https://demoblaze.com/index.html');
-    await expect(page).toHaveTitle('STORE');
-    await expect(page).toHaveURL('https://demoblaze.com/index.html');
+    await page.goto('https://demo.nopcommerce.com/');
+    await expect(page).toHaveTitle('nopCommerce demo store. Home page title');
+    await expect(page).toHaveURL('https://demo.nopcommerce.com/');
     const pageURL = page.url();
     console.log('Page URL is',pageURL);
 
